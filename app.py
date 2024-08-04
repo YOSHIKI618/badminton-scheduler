2fab88fff82c7baafe02b51d1d574b376f349f99
Yoshiki　Unno <yoshiki040618@gmail.com>
2024-08-05 00:37:44 +0900

    Initial commit

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 import random
+from urllib.parse import quote as url_quote
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///players.db'
